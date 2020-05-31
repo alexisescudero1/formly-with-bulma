01b263794dd06451c3c9997dce6d4952ababddc9
Alexis Escudero <alexisescudero1@gmail.com>
2020-05-31 13:57:52 -0300

    Auto-generated commit

--- src/app/formly/bulma/wrapper/formly-field-wrapper.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { FieldWrapper } from '@ngx-formly/core';
-
-@Component({
-  selector: 'app-formly-wrapper-form-field',
-  templateUrl: './formly-field-wrapper.html'
-})
-export class FormlyWrapperFormFieldComponent extends FieldWrapper {}
